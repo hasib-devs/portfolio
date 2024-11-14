f78454d294178cc55a41976559ccd05432fcdb72
Hasibur Rahman <hasib.webdev@gmail.com>
2024-11-14 22:26:03 +0600

    Add database seeders, models, factories, and migrations for posts, tags, categories, and comments

---
+++ app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /** @use HasFactory<\Database\Factories\PostFactory> */
+    use HasFactory;
+
+    protected $fillable = ['title', 'slug', 'content', 'user_id', 'published_at'];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(PostCategory::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(PostTag::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(PostComment::class);
+    }
+}
---
+++ app/Models/PostComment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostComment extends Model
+{
+    /** @use HasFactory<\Database\Factories\PostCommentFactory> */
+    use HasFactory;
+
+    protected $fillable = ['post_id', 'user_id', 'content'];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
